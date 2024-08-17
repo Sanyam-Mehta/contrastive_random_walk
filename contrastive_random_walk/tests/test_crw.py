51a2fb7cfb72d8c353c77d5d21ba7a560084c217
Sanyam Mehta <sanyam@Sanyams-MacBook-Pro.local>
2024-08-17 15:07:52 +0530

    test case modified

--- contrastive_random_walk/tests/test_crw.py
+++ contrastive_random_walk/tests/test_crw.py
@@ -4,14 +4,15 @@ from contrastive_random_walk.model.crw import ContrastiveRandomWalk
 
 def test_contrastive_random_walk():
     # Test case 1: Random input tensor
+    output_dim = 128
     model = ContrastiveRandomWalk(
-        resnet_type="resnet18", output_dim=128, temperature=1.0, edge_dropout_rate=0.5
+        resnet_type="resnet18", output_dim=output_dim
     )
     input_tensor = torch.randn(16, 5, 10, 64, 64, 3)  # (B, T, N, H, W, C)
-    output = model(input_tensor)  # output shape: (B, N, N)
+    output = model(input_tensor)  # output shape: (B, T, N, D)
 
     print(output.shape)
-    assert output.shape == (16, 10, 10)
+    assert output.shape == (16, 5, 10, output_dim)
 
     print("All test cases passed (ContrastiveRandomWalk)!")
 
