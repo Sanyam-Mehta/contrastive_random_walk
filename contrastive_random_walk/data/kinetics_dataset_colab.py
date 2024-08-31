2b742832558a237a638023e1d5fa23a5ebec4d03
Sanyam Mehta <sanyam@Sanyams-MacBook-Pro.local>
2024-08-31 18:04:10 -0400

    .

---
+++ contrastive_random_walk/data/kinetics_dataset_colab.py
@@ -0,0 +1,152 @@
+import time
+
+import numpy as np
+
+from contrastive_random_walk.utils import extract_patches_with_jitter, make_palindrome
+from torchvision.datasets.folder import make_dataset
+from torchvision.datasets.kinetics import Kinetics
+from torchvision.datasets.utils import list_dir
+
+from torchvision.datasets.video_utils import VideoClips
+
+
+class KineticsCustom():
+    def __init__(
+        self,
+        root,
+        frames_per_clip,
+        split="train",
+        step_between_clips=1,
+        frame_rate=None,
+        extensions=("mp4",),
+        _precomputed_metadata=None,
+        num_classes=400,
+        transform_video=None,
+        tranformations_frame=None,
+        return_palindrome=False,
+    ):
+        super(KineticsCustom, self).__init__()
+        self.classes = list(sorted(list_dir(root)))
+        self.class_to_idx = {self.classes[i]: i for i in range(len(self.classes))}
+        self.samples = make_dataset(
+            self.root, self.class_to_idx, extensions, is_valid_file=None
+        )
+        video_list = [x[0] for x in self.samples]
+        self.video_clips = VideoClips(
+            video_list,
+            frames_per_clip,
+            step_between_clips,
+            frame_rate,
+            _precomputed_metadata,
+        )
+        self.tranformations_frame = tranformations_frame
+        self.return_palindrome = return_palindrome
+
+    def __getitem__(self, idx):
+        # Get the video from the index
+        video = self.get_video_from_index(idx)
+        # video shape: (T, H, W, C) and channels dimension is last
+
+        # Transform each frame as follows:
+        # 1. Convert the frame to a 256*256 image
+        # 2. Convert each frame to a 7*7 grid of 64*64 patches using extract_patches_with_jitter function
+        # 3. Apply the transform on each patch
+
+        # The transformed video should have the shape (T, N, H, W, C)
+        # where N is the number of patches (49 in this case)
+        # H is the height of each patch (64)
+        # W is the width of each patch (64)
+        # C is the number of channels in the input tensor (3)
+
+        # Actual code begins here:
+        video_patches = []
+        for i in range(video.shape[0]):
+            # img shape is (H, W, C). It is a tensor of shape (64, 64, 3)
+            img = video[i]
+            _, modified_patches = extract_patches_with_jitter(
+                img,
+                transforms=self.tranformations_frame,
+            )
+            video_patches.append(modified_patches)
+
+        # # transform the list into a palindrome:
+        if self.return_palindrome:
+            video_patches = make_palindrome(video_patches)
+
+        video_patches = np.stack(video_patches)
+
+        # video shape: (T, H, W, C) and channels dimension is last
+        video = video.unsqueeze(1)  # T, NxN, H, W, C where N == 1
+
+        # video_patches has dimensions (2*clip_len/clip_len, 49, 64, 64, 3) [2*T, NxN, H, W, C]
+        return video_patches, video
+
+    def get_video_from_index(self, idx):
+        video, _, _, _ = self.video_clips.get_clip(idx)
+
+        # video shape: (T, H, W, C) and channels dimension is last
+        assert video.shape[3] == 3, "Video should have 3 channels"
+        return video  # video, audio, self.class_to_idx[info["label"]]
+
+
+class KineticsCustomTest:
+    def __init__(
+        self,
+        dummy_video,
+        transform_video=None,
+        tranformations_frame=None,
+        return_palindrome=False,
+    ):
+        self.dummy_video = dummy_video
+        self.transform_video = transform_video
+        self.tranformations_frame = tranformations_frame
+        self.return_palindrome = return_palindrome
+
+    def get_video_from_index(self, idx):
+        dummy_video = self.transform_video(self.dummy_video)
+        return dummy_video
+
+    def __getitem__(self, idx):
+        video = self.get_video_from_index(idx)
+        print("dummy video transofmration done")
+
+        # Direct copy from KineticsCustom.__getitem__ method
+        # Transform each frame as follows:
+        # 1. Convert the frame to a 256*256 image
+        # 2. Convert each frame to a 7*7 grid of 64*64 patches using extract_patches_with_jitter function
+        # 3. Apply the transform on each patch
+
+        # The transformed video should have the shape (T, N, H, W, C)
+        # where N is the number of patches (49 in this case)
+        # H is the height of each patch (64)
+        # W is the width of each patch (64)
+        # C is the number of channels in the input tensor (3)
+
+        # Actual code begins here:
+        time_start = time.time()
+        video_patches = []
+        for i in range(video.shape[0]):
+            # img shape is (H, W, C). It is a tensor of shape (64, 64, 3)
+            img = video[i]
+            _, modified_patches = extract_patches_with_jitter(
+                img,
+                transforms=self.tranformations_frame,
+            )
+            video_patches.append(modified_patches)
+
+        time_end = time.time()
+
+        # print time taken in seconds for patch extraction
+        print("Time taken for patch extraction in seconds: ", time_end - time_start)
+
+        # # transform the list into a palindrome:
+        if self.return_palindrome:
+            video_patches = make_palindrome(video_patches)
+
+        video_patches = np.stack(video_patches)
+
+        # video shape: (T, H, W, C) and channels dimension is last
+        video = video.unsqueeze(1)  # T, NxN, H, W, C where N == 1
+
+        # video_patches has dimensions (2*clip_len, 49, 64, 64, 3) [2*T, NxN, H, W, C]
+        return video_patches, video
---
+++ contrastive_random_walk/tools/train_colab.py
@@ -0,0 +1,99 @@
+import lightning as L
+import torch
+from contrastive_random_walk.data.kinetics_dataset_colab import KineticsCustom
+from contrastive_random_walk.model.crw import ContrastiveRandomWalkLightningWrapper
+from contrastive_random_walk.viz.visualizer import Visualizer
+from torchvision import transforms as T
+
+print("Training Started")
+
+# Initialize the visualizer
+visualizer = Visualizer(
+    tf_log=True,
+    use_html=True,
+    win_size=256,
+    name="contrastive_random_walk_train",
+    freq=100,  # every 100 epochs
+)
+
+# Initialize the model
+model = ContrastiveRandomWalkLightningWrapper(
+    resnet_type="resnet18",
+    output_dim=128,
+    temperature=1.0,
+    edge_dropout_rate=0.5,
+    learning_rate=1e-3,
+    visualizer=visualizer,
+)
+
+transforms_video = T.Compose(
+    [
+        T.Resize(256),
+        T.ColorJitter(),
+    ]
+)
+
+tranformations_frame = T.Compose(
+    [
+        # RandomResizedCrop is the spatial jitter transofrmation
+        T.RandomResizedCrop((64, 64), scale=(0.7, 0.9), ratio=(0.7, 1.3)),
+    ]
+)
+
+# TODO: Check if transforms are passed correctly
+train_dataset = KineticsCustom(
+    root="data/kinetics400",
+    split="train",
+    frames_per_clip=5,
+    step_between_clips=1,
+    frame_rate=None,
+    extensions=("mp4",),
+    num_classes=400,
+    transform_video=transforms_video,
+    tranformations_frame=tranformations_frame,
+)
+
+# val_dataset = KineticsCustom(
+#     root="data/kinetics400",
+#     split="val",
+#     frames_per_clip=5,
+#     step_between_clips=1,
+#     frame_rate=None,
+#     extensions=("mp4",),
+#     num_classes=400,
+#     transform_video=transforms_video,
+#     tranformations_frame=tranformations_frame,
+# )
+
+# test_dataset = KineticsCustom(
+#     root="data/kinetics400",
+#     split="test",
+#     frames_per_clip=5,
+#     step_between_clips=1,
+#     frame_rate=None,
+#     extensions=("mp4",),
+#     num_classes=400,
+#     transform_video=None,
+#     tranformations_frame=tranformations_frame,
+# )
+
+train_dataloader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=16, shuffle=True
+)
+val_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=False)
+# test_dataloader = torch.utils.data.DataLoader(
+#     test_dataset, batch_size=16, shuffle=False
+# )
+
+# Each element in the dataset is a tensor of size (2*T, NxN, H, W, C), where:
+# T: clip length
+# N: number of patches
+# H: height of each patch (64)
+# W: width of each patch (64)
+# C: number of channels in the input tensor (3)
+
+# Initialize the trainer
+trainer = L.Trainer(max_epochs=1)
+
+# Train the model
+trainer.fit(model, train_dataloader, val_dataloader)
